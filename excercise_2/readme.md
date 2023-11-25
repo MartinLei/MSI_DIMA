cecb9a154ceaa7b40626a6b8e0e48873712cc840
Stefan <stefan-ambs@web.de>
2023-11-25 15:23:57 +0100

    Change dict attack

---
+++ excercise_2/readme.md
@@ -0,0 +1,24 @@
+# System 1
+
+# System 2
+length: 10 signs
+allowed signs: uppercase and lowercase latin letters and digits 0 to 9 with repetitions
+SHA1-hash: e39156538e3e7193fdf6356469fa7414d078f97e
+
+839299365868340224 possible Passwords
+8.393 x 10^17
+eight hundred and thirty-nine quadrillion, two hundred and ninety-nine trillion, three hundred and sixty-five billion, eight hundred and sixty-eight million, three hundred and forty thousand, two hundred and twenty-four
+
+Estimated Time for Brute Force with 1.000.000 Checks per second: 26614 years, 3 days, 44 minutes, 28 seconds
+(My Laptop checks arround 600.000)
+
+## Dict attack
+Funfact: To save them all in a dict, it need 8.74 exabytes (one exabyte is 1 billion gigabytes)
+
+### 46.717 Checks random password list from the internet
+Password not found in dictionary
+Time taken: 0.2289142608642578 seconds for 46.717 checks.
+
+### 100.000 Checks from generated file which hope for Mrs. Stähles lazyness
+Password not found in dictionary
+Time taken: 0.13896846771240234 seconds for 100.000 checks.
--- excercise_2/system-2/system-2_dict.py
+++ excercise_2/system-2/system-2_dict.py
@@ -3,21 +3,24 @@
 import hashlib
 import time
 
-# First try dictonary attack 1_000_000 Problem: Password not in dictonary
-def check_password(hash, password):
-    return hashlib.sha1(password.encode()).hexdigest() == hash
-def dictonary_attack():
-    start_time = time.time()
-    hash_to_crack = "e39156538e3e7193fdf6356469fa7414d078f97e"
+hash_to_crack = "e39156538e3e7193fdf6356469fa7414d078f97e"
+#hash_to_crack = "fb909c8945c54c067fbdfd7da802633fd12e6a2b"
 
-    with open("password_1_000_000.txt", "r") as file:
-        dictionary = file.readlines()
+def check_password(password):
+    return hashlib.sha1(password.encode()).hexdigest() == hash_to_crack
+
+def dictonary_attack(dictionary):
+    start_time = time.time()
+    number_of_checks = 0
 
     password_found = False
     for word in dictionary:
-        if check_password(hash_to_crack, word):
-            password_found = True
-            break
+        word = word.replace("\n", "")
+        if len(word) == 10:
+            number_of_checks += 1
+            if check_password(word):
+                password_found = True
+                break
 
     if password_found == True:
         print(f"Password found: {word}")
@@ -26,13 +29,20 @@ def dictonary_attack():
 
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Time taken: {elapsed_time} seconds")
+    print(f"Time taken: {elapsed_time} seconds for {number_of_checks} checks.\n")
 #------------------------------------------------------------------------
 
 #Second try rainbow attack
 
 def main():
-    dictonary_attack()
+    with open("./system-2/password_1_000_000.txt", "r") as file:
+        dictionary = file.readlines()
+    dictonary_attack(dictionary)
+
+    # Try to attack with the assumption that Mrs Stähle was too lazy and simply used the first password and after this just numbers
+    with open("./system-2/password_system2_dict_FirstPasswordOnlyNumbers.txt", "r") as file:
+        dictionary = file.readlines()
+    dictonary_attack(dictionary)
     print("\n")
 
 if __name__ == "__main__":
